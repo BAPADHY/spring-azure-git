b45d59a6bf0a94099847527078b8f5a9ce8bbbbe
varsa <barshapadhy3@gmail.com>
2023-07-04 11:42:53 +0530

    files added

---
+++ src/test/java/com/cg/azure/SpringAzureGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
